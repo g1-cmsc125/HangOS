56010da68444d7b7f97c6af1b3ae2af21acd56f0
Mac Alvarico <macroninpalvarico@gmail.com>
2026-02-03 17:56:27 +0800

    commit1
    logic and very primitive frontend

---
+++ logicWithMinorFront/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.SwingUtilities;
+
+
+public class Main{
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+           MainFrame game = new MainFrame();
+            game.setVisible(true);
+        });
+    }
+}
---
+++ logicWithMinorFront/MainFrame.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.BorderLayout;
+
+public class MainFrame extends JFrame{
+    CardPanel cardPanel; 
+    public MainFrame() {
+        this.setTitle("Hangman");
+        this.setDefaultCloseOperation(3);
+        this.setSize(1000, 700);
+        this.setLayout(new BorderLayout());
+        cardPanel = new CardPanel(); 
+        this.add(cardPanel); 
+        cardPanel.showMenuPanel(); 
+
+        this.setLocationRelativeTo(null);
+        this.setResizable(false); 
+        
+    }
+}
